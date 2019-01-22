f4e4cf611f55a1aa345b6f12170e06ac5810a7f7
Sergi <snak_one@hotmail.com>
2019-01-22 18:53:38 +0100

    Update config.firebase.ts

--- src/config/config.firebase.ts
+++ src/config/config.firebase.ts
@@ -1,10 +1,10 @@
 export const environment = {
     firebase: {
-       apiKey: "AIzaSyC_dsWH5RS9ZU7ih8qtFGymxcUoDvb1_hU",
-       authDomain: "taxi-tracker-8f5f1.firebaseapp.com",
-       databaseURL: "https://taxi-tracker-8f5f1.firebaseio.com",
-       projectId: "taxi-tracker-8f5f1",
-       storageBucket: "taxi-tracker-8f5f1.appspot.com",
-       messagingSenderId: "935423656483"
+       apiKey: "APIKEY",
+       authDomain: "AUTHDOMAIN",
+       databaseURL: "DBURL",
+       projectId: "PROJECTID",
+       storageBucket: "STOREBUCKET",
+       messagingSenderId: "MESSAGINGSENDERID"
     }
   };
